c33e3b11e241d323d10eccda5291c9fc80579759
Vincent <vincent@vincents-air.univ.ad.cityu.edu>
2018-04-05 18:33:37 -0700

    Initial Commit

---
+++ cplusplus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cplusplus
+//
+//  Created by Vincent on 2018/4/5.
+//  Copyright © 2018年 Vincent. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
