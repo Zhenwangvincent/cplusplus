a57d1ac0facefc8aafc32688f0e9d7dc69517aef
Zhen Wang <zhenwangvincent@gmail.com>
2018-04-16 20:22:55 -0700

    add new content
    the new content is a little c++ code. The function is to show the phone name when the controller input a phone name.

--- cplusplus/main.cpp
+++ cplusplus/main.cpp
@@ -7,9 +7,24 @@
 //
 
 #include <iostream>
+#include "assignment1.h"
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    Phone myPhone; // create Account object myAccount
+    
+    // show that the initial value of myAccount's name is the empty string
+    cout << "Initial Phone name is: " << myPhone.getName();
+    
+    // prompt for and read name
+    cout << "\nPlease enter the Phone name: ";
+    string theName;
+    getline(cin, theName); // read a line of text
+    myPhone.setName(theName); // put theName in myPhone
+    
+    // display the name stored in object myPhone
+    cout << "Name in object myPhone is: "
+    << myPhone.getName() << endl;
     return 0;
 }
+
